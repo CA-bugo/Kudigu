579b9a1724ee9355f8883ac6a8d05318942c35a9
CA-bugo <untalanaeron2004@gmail.com>
2026-03-06 12:21:41 +0800

    Update userControllers.js

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -2,16 +2,15 @@ const connection = require('../config/db');
 
 // Get all users
 exports.getAllUsers = (req, res) => {
-  connection.query('SELECT * FROM inventory', (err, results) => {
-    if (err) {
-        return res.status(500).json({ error: 'Database query failed' });
-    }
-    res.json(results);
-  });
+connection.query('SELECT * FROM inventory', (err, results) => {
+if (err) {
+return res.status(500).json({ error: 'Database query failed' });
+}
+res.json(results);
+});
 };
 
-JavaScript
-Copy
+// Create a new user
 exports.createUser = (req, res) => {
   const { iname, uprice, quan, sup } = req.body; // Match frontend
     
